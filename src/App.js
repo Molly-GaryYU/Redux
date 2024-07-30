6853e8f982a8d31360ef87153fb2b764f4fd9f1d
Molly <9390548+MollyLove@user.noreply.gitee.com>
2024-07-30 10:42:43 +0800

    第二次

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import "./css-google/bottom.css";
 import './css-google/googleHome.css';
 import './css-google/search.css';
 import './css-google/navigation.css';
-import navContent from './js/navigation';
+import NavContent from './js/navigation';
 
 function App() {
   return (
     <div className="main-frame" id="main-frame">
      <div className="navigation">
-      <navContent />
+      <NavContent />
      </div>
      <div className='search'></div>
      <footer className="bottom"></footer>
--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,17 +1,107 @@
+import { useState } from 'react';
 import '../css-google/navigation.css';
-import ninePoint from "../img/img-googleninePoint.png";
+import ninePoint from "../img/img-google/ninePoint.png";
+import app1 from "../img/img-google/app1.png";
+import app2 from "../img/img-google/app2.png";
+import app3 from "../img/img-google/app3.png";
+import app4 from "../img/img-google/app4.png";
+import app5 from "../img/img-google/app5.png";
+import app6 from "../img/img-google/app6.png";
 
-export default function navContent(){
-  <nav className="navigation__nav">
+let box=[];
+let appDisplay=[];
+let firstTimeToClike=true;
+
+export default function NavContent(){
+  const [boxShow ,setBoxShow]=useState([]);
+  let [ninePointClikeOrNot,setNinePointClikeOrNot]=useState(false)
+    return (
+    <nav className="navigation__nav">
         <a className="navigation__nav--img" href="aaa" alt="">图片</a>
-                    <div className="navigation__nav--more " id="nine-point">
+                    <div onClick={DialogMoreApp(setBoxShow,setNinePointClikeOrNot)} className="navigation__nav--more " id="nine-point">
                         <img className="navigation__nav--more__img" src={ninePoint} alt="" />
                     </div>
-                    <div id="navigation__nav__dialog" className="navigation__nav__dialog" style="display: none;">
+                    <div  id="navigation__nav__dialog" className="navigation__nav__dialog" style={{display:ninePointClikeOrNot?'block':'none'}}>
                         <div id="navigation__nav__dialog--display" className="navigation__nav__dialog--display">
-                          {/* 内容填充 */}
+                            {boxShow}
                         </div>
                     </div>
                     <a className="navigation__nav--login__a navigation__nav--login">登录</a>
-  </nav>
+    </nav>
+  );
 }
+//点击时请求
+ function DialogMoreApp(setBoxShow,setNinePointClikeOrNot){
+  if(firstTimeToClike){
+    fetch("http://localhost:8080/pages/back/goods/getGoods").then((result)=>{
+      forEachApps(result,setBoxShow,setNinePointClikeOrNot);
+      firstTimeToClike=false;
+    }).catch(()=>{
+      forEachApps(moreData,setBoxShow,setNinePointClikeOrNot);
+      firstTimeToClike=false;
+    }
+    );
+  }
+}
+//组件间通信通过state（单机关闭搜索框）
+//大类（第一层遍历）
+function forEachApps(result,setBoxShow,setNinePointClikeOrNot){
+  setNinePointClikeOrNot(true);
+    result.forEach((elem)=>{
+    let appArray=Object.values(elem);
+    let box_a=AppBox(appArray)
+    box.push(box_a);
+    appDisplay=[];
+    box_a=[];
+  })
+  setBoxShow(box);
+}
+//小类（第二层遍历,appArray是一个二维数组，所以得遍历两次）
+function AppBox(appArray){
+   appArray.forEach((row) =>{
+    row.map((colume,i)=>{
+      console.log("i :",i)
+      appDisplay.push(
+        <a className='navigation__nav__dialog--display--type--box' href='' key={colume.id}>
+          <div className='navigation__nav__dialog--display--type--box__div'>
+            {colume.imgComponent}
+          </div>
+          <span>{colume.name}</span>
+        </a>)
+    })
+  })
+  console.log("appDisplay is",appDisplay)
+  return (<div className="navigation__nav__dialog--display--type" >
+    <div className='navigation__nav__dialog--display--type--child'>
+       {appDisplay}</div></div>)
+}
+
+const moreData=[{googleApp:[
+    {name:'账号' , imgComponent: <img src={app1} className='navigation__nav__dialog--display--type--box__img'  alt=''/>,id:"0"},
+    {name:'搜索' ,imgComponent: < img src={app2} className='navigation__nav__dialog--display--type--box__img'  alt=''/>,id:"1"},
+    {name:'搜索' ,imgComponent: < img src={app3} className='navigation__nav__dialog--display--type--box__img'  alt=''/>,id:"2"},
+    {name:'搜索' ,imgComponent: < img src={app6} className='navigation__nav__dialog--display--type--box__img'  alt=''/>,id:"3"},
+    {name:'搜索' ,imgComponent: < img src={app4} className='navigation__nav__dialog--display--type--box__img'  alt=''/>,id:"4"},
+    {name:'搜索' ,imgComponent: < img src={app5} className='navigation__nav__dialog--display--type--box__img'  alt=''/>,id:"5"},
+    {name:'搜索' ,imgComponent: < img src={app3} className='navigation__nav__dialog--display--type--box__img'  alt=''/>,id:"6"},
+    {name:'搜索' ,imgComponent: < img src={app5} className='navigation__nav__dialog--display--type--box__img'  alt=''/>,id:"7"},
+    {name:'搜索' ,imgComponent: < img src={app1} className='navigation__nav__dialog--display--type--box__img'  alt=''/>,id:"8"},
+    {name:'搜索' ,imgComponent: < img src={app2} className='navigation__nav__dialog--display--type--box__img'  alt=''/>,id:"9"},
+    {name:'地图' ,imgComponent: < img src={app6} className='navigation__nav__dialog--display--type--box__img'  alt=''/>,id:"10"}
+    ]}
+    ,{
+    anthorApp:[
+    {name:'新闻' ,  imgComponent: < img src={app4} className='navigation__nav__dialog--display--type--box__img'  alt=''/>,id:"11"},
+    {name:'Gemail' ,imgComponent: < img src={app5} className='navigation__nav__dialog--display--type--box__img'  alt=''/>,id:"12"},
+    {name:'Meet' ,  imgComponent: < img src={app6} className='navigation__nav__dialog--display--type--box__img'  alt=''/>,id:"13"},
+    {name:'Gemail' ,imgComponent: < img src={app1} className='navigation__nav__dialog--display--type--box__img'  alt=''/>,id:"14"},
+    {name:'Gemail' ,imgComponent: < img src={app2} className='navigation__nav__dialog--display--type--box__img'  alt=''/>,id:"15"},
+    {name:'Gemail' ,imgComponent: < img src={app3} className='navigation__nav__dialog--display--type--box__img'  alt=''/>,id:"16"},
+    {name:'Gemail' ,imgComponent: < img src={app2} className='navigation__nav__dialog--display--type--box__img'  alt=''/>,id:"17"},
+    {name:'Gemail' ,imgComponent: < img src={app3} className='navigation__nav__dialog--display--type--box__img'  alt=''/>,id:"18"},
+    {name:'Gemail' ,imgComponent: < img src={app4} className='navigation__nav__dialog--display--type--box__img'  alt=''/>,id:"19"},
+    {name:'Gemail' ,imgComponent: < img src={app2} className='navigation__nav__dialog--display--type--box__img'  alt=''/>,id:"20"},
+    {name:'Gemail' ,imgComponent: < img src={app6} className='navigation__nav__dialog--display--type--box__img'  alt=''/>,id:"21"},
+    {name:'Gemail' ,imgComponent: < img src={app5} className='navigation__nav__dialog--display--type--box__img'  alt=''/>,id:"22"},
+    {name:'Gemail' ,imgComponent: < img src={app2} className='navigation__nav__dialog--display--type--box__img'  alt=''/>,id:"23"}
+    ]},];
